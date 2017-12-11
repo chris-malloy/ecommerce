f1f2794ed61a35f15835e4b0c6ab90979315418f
chris-malloy <chrismalloymusic@gmail.com>
2017-12-11 14:10:18 -0500

    added navbar and slick slider

--- front-end/src/NavBar.js
+++ front-end/src/NavBar.js
@@ -6,7 +6,28 @@ class NavBar extends Component{
     }
     render(){
         return(
-            <h1>Navbar sanity check</h1>
+        <div id="navbar">
+            <nav className="light-green lighten-1" role="navigation">
+                <div className="nav-wrapper container">
+                    <a id="logo-container" href="/" className="brand-logo">Ecommerce</a>
+                    <ul id="nav-mobile" className="right hide-on-med-and-down">
+                        <li><a href="/">Thing 1</a></li>
+                        <li><a href="/">Thing 2</a></li>
+                        <li><a href="/">Thing 3</a></li>
+                        <li><a href="/">Thing 4</a></li>
+                    </ul>
+                </div>
+                <div className="nav-wrapper green">
+                    <div className="container center">
+                        ClassicModels Logo
+                    </div>
+                    <ul className="right">
+                        <li>Sign in or Create an account</li>
+                        <li>(0) items in cart | ($0.00)</li>
+                    </ul>
+                </div>
+            </nav>
+        </div>
         )
     }
 }
---
+++ front-end/src/components/SlickSlider.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+class SlickSlider extends Component{
+    render(){
+        return(
+            <h1>Slick SLider</h1>
+        )
+    }
+}
+export default SlickSlider;
