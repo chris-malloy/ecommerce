e1dfaadaa66a0a2cfc0f025ced936702d0570aea
chris-malloy <chrismalloymusic@gmail.com>
2017-12-11 12:30:16 -0500

    set up router and add navbar

---
+++ front-end/src/NavBar.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class NavBar extends Component{
+    constructor(){
+        super();
+    }
+    render(){
+        return(
+            <h1>Navbar sanity check</h1>
+        )
+    }
+}
+
+export default NavBar;
