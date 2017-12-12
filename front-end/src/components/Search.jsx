12c31539c83b874600d5244a8b02e596aec16c06
chris-malloy <chrismalloymusic@gmail.com>
2017-12-12 13:48:54 -0500

    tweaked Search

--- front-end/src/components/Search.jsx
+++ front-end/src/components/Search.jsx
@@ -1,21 +1,16 @@
 import React, { Component } from 'react';
 // materialize components
 
-
-
 class Search extends Component{
     render(){
         return(
             <div id="search" className="row">
                 <form action="" className="col s12">
                     <div className="row">
-                        <div className="input-field col s6">
+                        <div className="input-field col s12">
                             <i className="material-icons prefix green-text text-darken-1">search</i>
                             <input type="text" className="validate" id="icon_prefix" /> 
                             <label htmlFor="icon_prefix">Search</label>
-                        </div>
-                        <div className="input-field col s6">
-                            <a className="waves-effect waves-light btn green darken-1">Search</a>
                         </div>
                     </div>
                 </form>
