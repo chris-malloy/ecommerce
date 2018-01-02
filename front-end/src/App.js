405119e5e9939b96d9840f8a463e4d9668e8a61f
chris-malloy <chrismalloymusic@gmail.com>
2018-01-02 09:38:40 -0500

    chagne to app.js

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+
 // components
 import NavBar from './containers/NavBar';
 import SlickSlider from './containers/SlickSlider';
